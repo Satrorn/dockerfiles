4c111a5812c1ee25c3fd92551ec0ac742d46546d
Satrorn <satrorn@gmail.com>
2022-11-10 18:40:24 +0800

    提交代码：2022-11-10 18:40:20

---
+++ frps/README.md
@@ -0,0 +1,6 @@
+## 简介
+基于 [fatedier/frp](https://github.com/fatedier/frp) 项目的 docker 镜像.
+
+## 使用
+使用教程和一键脚本请参考 [stilleshan/frps](https://github.com/stilleshan/frps) 项目
+
---
+++ proxypoolcheck/git_push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#脚本功能：从提交最新代码到git
+pwddir=$(cd $(dirname $0); pwd)
+echo $pwddir
+cd $(dirname $0)
+git remote add origin git@github.com:Satrorn/proxypoolcheck-docker.git
+git add .
+git commit -m "Up Commit"
+git push -u origin master
