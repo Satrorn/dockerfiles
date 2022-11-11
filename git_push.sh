9b29ac075fc6ef8797fa943fdd8a3f72408e7da2
Satrorn <satrorn@gmail.com>
2022-11-11 13:27:45 +0800

    提交代码：2022-11-11 13:27:41

--- git_push.sh
+++ git_push.sh
@@ -2,12 +2,12 @@
 #脚本功能：从提交最新代码到git
 ssh -T git@github.com
 remark=$(date +"%Y-%m-%d %H:%M:%S")
+echo ".登陆状态"
 git remote -v
 git add .
-echo "---开始下载---合并网络版本---"
 git pull origin
-echo "---下载完成---开始提交更新版本---"
+echo ".文件下载完成"
 git commit -m "提交代码：${remark}"
 git push -u origin master
 git reset --hard origin/master
-echo "---提交完成---结束---"
+echo ".文件更新完成"
