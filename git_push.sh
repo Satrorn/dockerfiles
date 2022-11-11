d4968bf9e1634ddee84bacd2c9c5dbbd19dfe239
Satrorn <satrorn@gmail.com>
2022-11-11 13:24:23 +0800

    提交代码：2022-11-11 13:24:19

--- git_push.sh
+++ git_push.sh
@@ -4,8 +4,10 @@ ssh -T git@github.com
 remark=$(date +"%Y-%m-%d %H:%M:%S")
 git remote -v
 git add .
+echo "---开始下载---合并网络版本---"
 git pull origin
+echo "---下载完成---开始提交更新版本---"
 git commit -m "提交代码：${remark}"
 git push -u origin master
 git reset --hard origin/master
-echo "---END---"
+echo "---提交完成---结束---"
